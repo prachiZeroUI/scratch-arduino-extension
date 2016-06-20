5c4e85eecf75be1bb4d3fe68eb947439469e93a3
Prachi Nawathe <pnnawathe@gmail.com>
2016-06-20 16:43:04 -0700

    comments

--- arduino_extension.js
+++ arduino_extension.js
@@ -507,7 +507,9 @@
     device.open({ stopBits: 0, bitRate: 57600, ctsFlowControl: 0 });
     console.log('Attempting connection with ' + device.id);
     device.set_receive_handler(function(data) {
+      console.log('probe ' + data);
       var inputData = new Uint8Array(data);
+      console.log(inputData);
       processInput(inputData);
     });
 
